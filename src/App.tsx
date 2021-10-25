c07d06979bbcddc619f78426192d71da8c592ced
Zoltan Rendes <rendes.zoltan@gmail.com>
2021-10-25 20:05:13 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<h2>Hello from React!</h2>, root);
+}
+
+render();
